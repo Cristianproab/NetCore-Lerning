062f67fa35b588932f813d379320ef238956a5ad
calvarado <cristianproab@gmail.com>
2022-05-25 09:46:15 -0500

    new

---
+++ NetCore-Lerning/CoreEscuela/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace CoreEscuela
+{
+    class Escuela
+    {
+        public string nombre = "";
+        public string direccion = "";
+        public int añoFundacion;
+
+        public string ceo = "Camilo Alvarado";
+
+        public void Timbrar()
+        {
+            //todo
+            Console.Beep(400,3000);
+        }
+    }
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "Platzi Academy";
+            miEscuela.direccion = "Bogota Colombia";
+            miEscuela.añoFundacion = 2012;
+            Console.WriteLine("Timbre");
+            miEscuela.Timbrar();
+        }
+        /* {Console.WriteLine("Hello, World!");} */
+    }
+}
+
