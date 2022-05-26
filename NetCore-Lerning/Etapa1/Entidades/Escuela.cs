c3f0dc1ddf850bb38468d27e3c3f223442512971
calvarado <cristianproab@gmail.com>
2022-05-26 15:26:52 -0500

    modified

--- NetCore-Lerning/Etapa1/Entidades/Escuela.cs
+++ NetCore-Lerning/Etapa1/Entidades/Escuela.cs
@@ -5,8 +5,25 @@ namespace CoreEscuela.Entidades
         string nombre;
         public string Nombre
         {
-            get{ return nombre; }
+            get{ return "Copia:" + nombre; }
             set {nombre = value.ToUpper();}
         }
+        public int AñoDeCreacion {get;set;}
+        public string Pais { get; set;}
+        public string Ciudad { get; set; }
+        public TiposEscuela TipoEscuela{ get; set; }
+        public Escuela(string nombre, int año) => (Nombre, AñoDeCreacion) = (nombre, año);
+        public Escuela(string nombre, int año, TiposEscuela tipo, string pais="", string ciudad ="")
+        {
+            (Nombre,AñoDeCreacion) = (nombre,año);
+            Pais = pais;
+            Ciudad = ciudad;
+        }
+
+        public override string ToString()
+        {
+            return $"Nombre: {Nombre}, Tipo: {TipoEscuela}, \n Pais: {Pais}, Ciudad: {Ciudad}";
+        }
+        
     }
 }
--- NetCore-Lerning/Etapa1/Program.cs
+++ NetCore-Lerning/Etapa1/Program.cs
@@ -1,2 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using CoreEscuela.Entidades;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Platzi Academy", 2022, TiposEscuela.Primaria, pais:"Colombia", ciudad:"Bogota");
+            escuela.Nombre = "Platzi Academy";
+            escuela.Pais = "Colombia";
+            escuela.Ciudad = "Bogota";
+            escuela.TipoEscuela = TiposEscuela.Primaria;
+
+            var arreglosCursos = new Curso[3];
+
+            arreglosCursos [0] = new Curso()
+                    {
+                        Nombre = "101"
+                    };
+            var curso2 = new Curso(){
+                Nombre = "201"
+            };
+            arreglosCursos [1] = curso2;
+            arreglosCursos [2] = new Curso
+                    {
+                        Nombre = "301"
+                    };
+
+
+            Console.WriteLine(escuela);
+            System.Console.WriteLine("=============");
+            Console.WriteLine(curso1.Nombre + "," + curso1.UniqueId);
+            Console.WriteLine(curso2.Nombre + "," + curso2.UniqueId);
+            Console.WriteLine($"{curso3.Nombre} , {curso3.UniqueId}");
+            Console.WriteLine(curso3);
+            
+        }
+    }
+}
+
+
