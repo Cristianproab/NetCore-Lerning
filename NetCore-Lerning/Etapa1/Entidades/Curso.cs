5cca5070319f8a775ff924676cea77d0924ac297
calvarado <cristianproab@gmail.com>
2022-05-26 15:25:48 -0500

    cambios

---
+++ NetCore-Lerning/Etapa1/Entidades/Curso.cs
@@ -0,0 +1,11 @@
+namespace CoreEscuela.Entidades
+{
+    public class Curso
+    {
+     public string UniqueId {  get; private set;}
+     public string Nombre { get; set; }   
+     public TiposJornada Jornada  { get; set; }
+     public Curso () => UniqueId = Guid.NewGuid().ToString();
+     
+    }
+}
