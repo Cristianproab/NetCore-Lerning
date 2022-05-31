4e7d294f440a56b64165d1294aecb17d31d067d6
calvarado <cristianproab@gmail.com>
2022-05-31 16:58:19 -0500

    cambios

--- NetCore-Lerning/Etapa1/Program.cs
+++ NetCore-Lerning/Etapa1/Program.cs
@@ -8,35 +8,68 @@ namespace Etapa1
     {
         static void Main(string[] args)
         {
-            var escuela = new Escuela("Platzi Academy", 2022, TiposEscuela.Primaria, pais:"Colombia", ciudad:"Bogota");
-            escuela.Nombre = "Platzi Academy";
-            escuela.Pais = "Colombia";
-            escuela.Ciudad = "Bogota";
-            escuela.TipoEscuela = TiposEscuela.Primaria;
+            var escuela = new Escuela("Platzi Academy", 2022, TiposEscuela.Primaria,
+             ciudad: "Bogota", pais: "Colombia"
+            );
 
-            var arreglosCursos = new Curso[3];
-
-            arreglosCursos [0] = new Curso()
-                    {
-                        Nombre = "101"
-                    };
-            var curso2 = new Curso(){
-                Nombre = "201"
+            escuela.Cursos = new Curso[]{
+                new Curso(){ Nombre = "101"},
+                new Curso(){ Nombre = "201"},
+                new Curso{ Nombre = "301"}
             };
-            arreglosCursos [1] = curso2;
-            arreglosCursos [2] = new Curso
-                    {
-                        Nombre = "301"
-                    };
+            ImprimirCusosEscuela(escuela);
+        }
 
+        private static void ImprimirCusosEscuela(Escuela escuela)
+        {
+            Console.WriteLine("====================");
+            Console.WriteLine("Cursos de la Escuela");
+            Console.WriteLine("====================");
 
-            Console.WriteLine(escuela);
-            System.Console.WriteLine("=============");
-            Console.WriteLine(curso1.Nombre + "," + curso1.UniqueId);
-            Console.WriteLine(curso2.Nombre + "," + curso2.UniqueId);
-            Console.WriteLine($"{curso3.Nombre} , {curso3.UniqueId}");
-            Console.WriteLine(curso3);
-            
+            foreach (var curso in escuela.Cursos)
+            {
+                Console.WriteLine($"Nombre {curso.Nombre}, id {curso.UniqueId}");
+            }
+        }
+
+        /* 
+                    Console.WriteLine(escuela);
+                    System.Console.WriteLine("============="); */
+        /*  Console.WriteLine(curso1.Nombre + "," + curso1.UniqueId);
+         Console.WriteLine(curso2.Nombre + "," + curso2.UniqueId);
+         Console.WriteLine($"{curso3.Nombre} , {curso3.UniqueId}");
+         Console.WriteLine(curso3); */
+
+        /* ImprimirCursosWhile(arreglosCursos);
+        System.Console.WriteLine("=============");
+        ImprimirCursosFor(arreglosCursos);
+        System.Console.WriteLine("=============");
+        ImprimirCursosForEach(arreglosCursos);
+    } */
+
+        private static void ImprimirCursosWhile(Curso[] arreglosCursos)
+        {
+            int contador = 0;
+
+            while (contador < arreglosCursos.Length)
+            {
+                Console.WriteLine($"Nombre {arreglosCursos[contador].Nombre}, id {arreglosCursos[contador].UniqueId}");
+                contador++;
+            }
+        }
+        private static void ImprimirCursosFor(Curso[] arregloCursos)
+        {
+            for (int i = 0; i < arregloCursos.Length; i++)
+            {
+                Console.WriteLine($"Nombre {arregloCursos[i].Nombre}), id {arregloCursos[i].UniqueId}");
+            }
+        }
+        private static void ImprimirCursosForEach(Curso[] arregloCursos)
+        {
+            foreach (var Curso in arregloCursos)
+            {
+                Console.WriteLine($"Nombre {Curso.Nombre}), id {Curso.UniqueId}");
+            }
         }
     }
 }
